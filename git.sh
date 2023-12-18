619b41eaff4284b5c59688069090270427536ca1
Anthony Gbobo <psalmseins@gmail.com>
2023-12-17 18:42:22 -0800

    0x19. C - Stacks, Queues - LIFO, FIFO

--- git.sh
+++ git.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-git add . && git commit -m "0x19. C - Stacks, Queues - LIFO, FIFO" && git push -u origin master
+git add . && git commit -m "0x19. C - Stacks, Queues - LIFO, FIFO" && git push
